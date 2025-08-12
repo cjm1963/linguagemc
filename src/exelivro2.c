64bbc4c10cd476bdc0d8b50975e9fd248cd02cc7
Claudio <claudiojm63@gmail.com>
2025-08-12 12:54:07 +0000

    exercicio do livro

---
+++ src/exelivro2.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+# include <stdlib.h>
+int main(){
+    system("clear");
+    int n;
+    printf("digite um numero\n");
+    scanf("%d",&n);
+
+    int i;
+    for (i = 100 ; i >= 0 ; i--){
+        printf("%d\n",i);
+    }
+    return 0;
+}
